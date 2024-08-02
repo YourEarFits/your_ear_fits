b9860df7e69aff851d2a4846ce5c96c24d043fed
Halozhan <juwonhwang0411@gmail.com>
2024-08-02 11:55:12 +0900

    로그인 기능과 로그아웃 기능 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:your_ear_fits/firebase_options.dart';
@@ -14,10 +14,102 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        body: Text("Hello World!",
+    return MaterialApp(
+      home: LoginScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            FirebaseAuth.instance.signOut();
+            Navigator.pop(context);
+          },
+          child: const Text('Logout'),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _login() async {
+    final String email = _emailController.text;
+    final String password = _passwordController.text;
+
+    print('Email: $email, Password: $password');
+    // Firebase Auth 로그인 로직
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (e) {
+      print('Login Failed: $e');
+      // Handle the exception here
+    }
+
+    // 로그인 성공 확인
+    final User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      print('Login Success: ${user.uid}');
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    } else {
+      print('Login Failed');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'E-mail',
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _login,
+              child: Text('Login'),
+            ),
+          ],
         ),
       ),
     );
@@ -30,12 +122,13 @@ class MyBase {
       options: DefaultFirebaseOptions.currentPlatform,
     );
 
-    FirebaseFirestore firestore = FirebaseFirestore.instance;
-    await firestore.collection("Users").doc("123456789").set(
-      {
-        "E-mail": "test@test.com",
-        "Name": "Kevin",
-      },
-    );
+    // Firebase Auth 상태 변화 감지
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
   }
 }
