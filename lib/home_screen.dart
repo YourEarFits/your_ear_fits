a22ee4b42621fa50f98dfa35179452bee8c221db
fryti112 <fryti112@gmail.com>
2024-08-08 11:11:53 +0900

    이벤트 버튼을 누르면 이벤트 페이지로 가는 기능 추가

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:your_ear_fits/my_page_screen.dart';
+import 'package:your_ear_fits/event_page_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -71,6 +72,14 @@ class HomeScreen extends StatelessWidget {
                   Column(
                     children: [
                       IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const EventPageScreen(),
+                            ),
+                          );
+                        },
                         icon: const Icon(Icons.event, size: 50),
                       ),
                       const Text('이벤트'),
