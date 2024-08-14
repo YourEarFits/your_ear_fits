1b5666dc48b40fcb82f96672df2877039bc168ab
fryti112 <fryti112@gmail.com>
2024-08-14 10:28:31 +0900

    이벤트 페이지 배경색 수정

---
+++ lib/ear_test_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class EarTestScreen extends StatelessWidget {
+  const EarTestScreen({super.key});
+  // 이벤트 페이지
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text('귀 검사'),
+      ),
+        backgroundColor: Color(0xFFFAFAFA),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          ],
+        ),
+      ),
+    );
+  }
+}
